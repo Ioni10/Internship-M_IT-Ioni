2125266b7a3678ae1264e194b3a31e2cf405d493
ManduIon <manduioncostinel@gmail.com>
2025-12-08 17:24:50 +0200

    "2st"

--- main.py
+++ main.py
@@ -16,3 +16,45 @@ df["region_availability"] = df["region_availability"].str.strip()
 df = df.rename(columns={"region_availability": "region"})
 
 df.to_csv("product_master_catalog.csv", index=False)
+
+
+
+
+
+"""Metrici de baza pe regiuni"""
+
+#Total SKU per region:
+total_sku_region = df.groupby("region")["sku"].nunique()
+
+#Media stock.urilor per SKU per region:
+avg_stock_region = df.groupby(["region", "sku"])["stock_total_units"].sum().groupby("region").mean()
+
+#Distributia pe categorie per region:
+dist_categories = df.groupby(["region", "category"])["sku"].nunique()
+
+
+#Crearea Tabelelor Pivot:
+#Numar de SKU-uri per category per region:
+pivot_count = df.pivot_table(
+    index="region",
+    columns="category",
+    values="sku",
+    aggfunc="count",
+    fill_value=0
+)
+#print("Tabel pivot Suma stock total\n",pivot_count)
+#print("-"*40)
+
+#Suma stock_total_units (pivot = stock per category per region)
+pivot_stock = df.pivot_table(
+    index="region",
+    columns="category",
+    values="stock_total_units",
+    aggfunc="sum",
+    fill_value=0
+)
+
+#print("Tabel pivot Numar de SKU-uri:\n",pivot_stock)
+#print("-"*40)
+
+
